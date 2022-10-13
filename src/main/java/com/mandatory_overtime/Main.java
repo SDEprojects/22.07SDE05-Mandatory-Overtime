d3d36feaf7bab2a9555c354e42414073d2d2a91c
blworth <worthambrandon@gmail.com>
2022-10-13 18:58:29 -0500

    created Text Parser, AB#381

---
+++ src/main/java/com/mandatory_overtime/Main.java
@@ -0,0 +1,15 @@
+package com.mandatory_overtime;
+
+
+import com.mandatory_overtime.controller.GamePlay;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    GamePlay gamePlay = new GamePlay();
+    gamePlay.printGameIntroduction();
+    gamePlay.gamePlayParser();
+
+  }
+}
