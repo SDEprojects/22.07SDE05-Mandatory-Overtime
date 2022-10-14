a47e7c0b5dea26f06b825187f7d4426c0b1acad7
chrogan <chrogan@amazon.com>
2022-10-14 14:34:32 -0700

    JSONfile resource continued, AB#389

--- src/main/java/com/mandatory_overtime/Main.java
+++ src/main/java/com/mandatory_overtime/Main.java
@@ -2,11 +2,16 @@ package com.mandatory_overtime;
 
 
 import com.mandatory_overtime.controller.GamePlay;
+import com.mandatory_overtime.model.JSONProcessor;
+
 
 
 public class Main {
 
   public static void main(String[] args) {
+    JSONProcessor newobject = new JSONProcessor();
+    newobject.jsonFileReader();
+
     GamePlay gamePlay = new GamePlay();
     gamePlay.newGame();
 
