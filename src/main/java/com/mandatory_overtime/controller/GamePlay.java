9e70e4abe45a434d10e54c9355d68649600c3b20
blworth <worthambrandon@gmail.com>
2022-10-14 11:54:53 -0500

    added quit method and rearranged other methods outside of gamePlay class, AB#380

--- src/main/java/com/mandatory_overtime/controller/GamePlay.java
+++ src/main/java/com/mandatory_overtime/controller/GamePlay.java
@@ -13,6 +13,9 @@ import java.util.stream.Stream;
 
 public class GamePlay {
 
+  private String verb;
+  private String noun;
+
   public final BufferedReader inputParser = new BufferedReader(new InputStreamReader(System.in));
   public String[] userInputArray;
 
@@ -20,9 +23,7 @@ public class GamePlay {
 
   public void printGameIntroduction() {
     try {
-      //      Instantiate UserView
-
-//      print startup title info
+      //      print startup title info
       System.out.println(userView.startUpTitle());
       System.out.println(userView.showHelp());
 
@@ -31,39 +32,59 @@ public class GamePlay {
       String name = inputParser.readLine().trim();
       System.out.println(userView.startUpInfo(name));
 
-
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
-//This method is parsing the user's input taking first word and assigning
-// it to verb and the last word assigning to noun
+
   public void gamePlayParser() {
-    String verb;
-    String noun;
+//    this parses userInput into 1st word == verb and last word == noun
     try {
-      do {
         userInputArray = inputParser.readLine().trim().split(" ");
         verb = userInputArray[0];
-        noun = userInputArray[userInputArray.length -1];
-//        System.out.println(Arrays.toString(userInputArray));
-//        System.out.println(verb + noun);
-        if (verb.equals("help")) {
-          System.out.println(userView.showHelp());
-        } else if (verb.equals("new")){
-            newGame();
-        }else {
-          System.out.println(userView.incorrectInput() + userView.showHelp());
-        }
-      } while (true);
-
+        noun = userInputArray[userInputArray.length - 1];
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
-  public void newGame(){
+
+
+  public void newGame() {
     GamePlay restart = new GamePlay();
     restart.printGameIntroduction();
-    restart.gamePlayParser();
+    restart.gamePlayCommands();
+  }
+
+public void quit() {
+//    this prompts user whether they want to quit continue or new game
+  System.out.println(
+      "Are you sure you would like to quit, start a new game or would you like to continue your game? \n(Enter: 'quit', 'new game', or 'continue game')");
+  while(true) {
+    gamePlayParser();
+    if (verb.equals("quit")) {
+      System.exit(0);
+    } else if (verb.equals("new")) {
+      newGame();
+    } else if (verb.equals("continue")) {
+      break;
+    }
+    System.out.println("Please type 'quit', 'new game' or 'continue game'");
   }
 }
+  public void gamePlayCommands() {
+//this parses commands to controller and view using conditionals
+    do {
+      gamePlayParser();
+      if (verb.equals("help")) {
+        System.out.println(userView.showHelp());
+      } else if (verb.equals("quit")) {
+        quit();
+      } else {
+        System.out.println(userView.incorrectInput() + userView.showHelp());
+      }
+    } while (true);
+
+  }
+}
+
+
