ebd00f2ddf6109346bf9581bcd331e1e1e674227
LashayC <cokleylashay@gmail.com>
2022-10-18 13:06:02 -0400

    Edited Main for merge conflict, AB#398

--- src/main/java/com/mandatory_overtime/Main.java
+++ src/main/java/com/mandatory_overtime/Main.java
@@ -1,6 +1,7 @@
 package com.mandatory_overtime;
 
 
+import com.mandatory_overtime.controller.GamePlay;
 import com.mandatory_overtime.model.Room;
 import java.util.HashMap;
 
@@ -8,25 +9,8 @@ import java.util.HashMap;
 public class Main {
 
   public static void main(String[] args) {
-//
-//    GamePlay gamePlay = new GamePlay();
-//    gamePlay.newGame();
-    Room objects = new Room();
-    objects.jsonFileReader();
-    HashMap<String, String[]> directions = objects.getLocationsDirections();
 
-    HashMap<String, String> items = objects.getLocationItems();
-
-    HashMap<String, String> NPC = objects.getLocationNPCs();
-
-    HashMap<String, String> display = objects.getLocationDisplayName();
-
-    String[] elevatorDirections = directions.get("elevator");
-
-    System.out.println(elevatorDirections[3]);
-    System.out.println(items.get("desk"));
-    System.out.println(NPC);
-    System.out.println(display.get("5"));
-
+    GamePlay gamePlay = new GamePlay();
+    gamePlay.newGame();
   }
 }
