c6c509d88ecb243b780f80271959cfaf3c8babe5
LashayC <cokleylashay@gmail.com>
2022-10-21 12:14:36 -0400

    Created gameSave method. Updated GamePlay class and additional save game menu options, AB#407

--- src/main/java/com/mandatory_overtime/Main.java
+++ src/main/java/com/mandatory_overtime/Main.java
@@ -10,7 +10,7 @@ public class Main {
   public static void main(String[] args) throws IOException{
 
     GamePlay gamePlay = new GamePlay();
-    gamePlay.startGame();
+//    gamePlay.startGame();
 
   }
 }
--- src/main/java/com/mandatory_overtime/controller/GamePlay.java
+++ src/main/java/com/mandatory_overtime/controller/GamePlay.java
@@ -6,7 +6,6 @@ import com.mandatory_overtime.view.UserView;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class GamePlay {
 
@@ -18,37 +17,101 @@ public class GamePlay {
   private String verb;
   private String noun;
 
+  /**
+   * Constructor for Gameplay that starts newGame();
+   * @throws IOException
+   */
   public GamePlay() throws IOException {
-    building = new Building();
+    newGame();
   }
 
-  public void startGame() throws IOException {
-    building.newGame();
+  /**
+   * THIS METHOD STARTS A NEWGAME FROM INTRODUCTION
+   * Starts gameIntro and then startmenu() start from new or start from save.
+   * @throws IOException
+   */
+  public void newGame() throws IOException {
+    printGameIntroduction();
+    startMenu();
   }
 
+  /**
+   * Prints game intro Ascii art and game commands.
+   */
   public void printGameIntroduction() {
-    try {
-      //      print startup title info
-      System.out.println(userView.startUpTitle());
-      System.out.println(userView.showHelp());
-      //      print game info and username
-      System.out.println("What is your name");
+  //      print startup title info
+  System.out.println(userView.startUpTitle());
+  System.out.println(userView.showHelp());
+}
 
+  /**
+   * Asks if user wants to start game from new or game from previous game
+   * @throws IOException
+   */
+  public void startMenu() throws IOException {
+    System.out.println("\n-------------------------------------Game Menu---------------------------------------------\n"
+        + "Would you like to start a 'new game' or 'previous game'?\n"
+        + "(Enter: 'new' or 'previous')\n");
+    System.out.print(">");
+    do{
+      gamePlayParser();
+      if(verb.equals("new")){
+        startGameFromNew();
+      } else if (verb.equals("previous")) {
+        startGameFromSaved();
+      }else{
+        System.out.println("Please enter 'new game' or a 'previous game'.");
+      }
+    }while(!verb.equals("new") && !verb.equals("previous"));
+
+  }
+
+  /**
+   * Starts game from New and asks name
+   * @throws IOException
+   */
+  public void startGameFromNew() throws IOException {
+    building = new Building();
+    try {
+      //Ask if user wants to start new game, continue from previous game
+
+      //Asks name and formats
+      System.out.println("What is your name");
       String name = inputParser.readLine().trim();
       String nameTemp;
       while (name.length() < 1) {
         nameTemp = inputParser.readLine().trim();
         name = nameTemp;
       }
+
+      //Updates Player instance name
       building.setName(name);
+
+      //prints startup Info
       System.out.println(userView.startUpInfo(name));
-      building.startingRoomDescription();
+
       //run printRoomDescription() here to show current room.
+      building.startingRoomDescription();
+
+      //start game loop
+      gamePlayCommands();
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
 
+  /**
+   * Starts game from saved (not complete)
+   * @throws IOException
+   */
+  public void startGameFromSaved() throws IOException {
+    System.out.println("method hasn't been created yet.");
+    startGameFromNew();
+  }
+
+  /**
+   * Parses user input for game
+   */
   public void gamePlayParser() {
 //    this parses userInput into 1st word == verb and last word == noun
     try {
@@ -60,25 +123,32 @@ public class GamePlay {
     }
   }
 
-
-
+  /**
+   * Quit menu parser that gives different options and sends methods.
+   * @throws IOException
+   */
   public void quitMenu() throws IOException {
 //    this prompts user whether they want to quit continue or new game
     System.out.println(
-        "Are you sure you would like to quit, start a new game or would you like to continue your game? \n(Enter: 'quit', 'new game', or 'continue game')");
+        "Are you sure you would like to quit, save & quit, start a new game or continue your game? \n(Enter: 'quit', 'save quit', 'new game', or 'continue game')");
     while (true) {
       gamePlayParser();
       if (verb.equals("quit")) {
         building.quit();
       } else if (verb.equals("new")) {
-        building.newGame();
-      } else if (verb.equals("continue")) {
+        newGame(); //building.newGame();
+      } else if(verb.equals("save")){
+        building.gameSave();
+      }else if (verb.equals("continue")) {
         break;
       }
       System.out.println("Please type 'quit', 'new game' or 'continue game'");
     }
   }
 
+  /**
+   * Takes user input to run game
+   */
   public void gamePlayCommands() {
 //this parses commands to controller and view using conditionals
     do {
